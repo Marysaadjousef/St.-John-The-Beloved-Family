b1e32e01567d2b2a5ebe72fd7333e40c5acb9223
Mary Saad <92622235+Marysaadjousef@users.noreply.github.com>
2024-10-09 10:54:38 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\flutter_windows_3.3.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\AndroidStudioProjects\\habat_khardal" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\flutter_windows_3.3.4-stable\\flutter"
+  "PROJECT_DIR=D:\\AndroidStudioProjects\\habat_khardal"
+  "FLUTTER_ROOT=E:\\flutter_windows_3.3.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\AndroidStudioProjects\\habat_khardal\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\AndroidStudioProjects\\habat_khardal"
+  "FLUTTER_TARGET=D:\\AndroidStudioProjects\\habat_khardal\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yZTRiYTljNmZiNDk5Y2NkNGU4MTQyMDU0Mzc4M2NjNzI2N2FlNDA2Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\AndroidStudioProjects\\habat_khardal\\.dart_tool\\package_config.json"
+)
